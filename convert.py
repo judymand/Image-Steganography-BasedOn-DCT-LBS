fec498b46c75be96e0e4262f6c29fcacdeb3fbf3
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-06-01 14:53:37 +0300

    Added DCT code and LSB code.

---
+++ convert.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+im1 = Image.open(r'..\JPEG-Image-compresion-python-example-master\Decompressed.jpg')
+im1.save(r'..\JPEG-Image-compresion-python-example-master\Decompressed.png')
